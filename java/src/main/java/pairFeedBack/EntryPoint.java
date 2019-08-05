0b4e0f17f18416702e3203364698f9f979eb3b8e
Hugarty Pedro <hp-ca@hotmail.com>
2019-08-05 10:57:56 -0300

    Adicionando properties que dão suporte para db, test e autenticação

--- java/src/main/java/pairFeedBack/EntryPoint.java
+++ java/src/main/java/pairFeedBack/EntryPoint.java
@@ -2,8 +2,10 @@ package pairFeedBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:security.properties")
 public class EntryPoint {
 
     public static void main(String[] args) {
