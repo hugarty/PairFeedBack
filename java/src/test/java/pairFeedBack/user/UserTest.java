803cb6af21ae57e4ffa54baccd3ee97cc80da71b
Hugarty Pedro <hp-ca@hotmail.com>
2019-08-06 14:40:58 -0300

    Adicionando teste que verifica acesso de usuário recem criado

--- java/src/test/java/pairFeedBack/user/UserTest.java
+++ java/src/test/java/pairFeedBack/user/UserTest.java
@@ -17,7 +17,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import pairFeedBack.dataTransferer.dto.TokenDto;
 import pairFeedBack.dataTransferer.dto.UserDto;
+import pairFeedBack.dataTransferer.form.LoginForm;
 import pairFeedBack.dataTransferer.form.SignUpForm;
 import pairFeedBack.utils.Utils;
 
@@ -25,32 +27,43 @@ import pairFeedBack.utils.Utils;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserTest {
 
-    @Value("${api.test.parameters.email.user}")
-    String userEmail;
-
-    @Value("${api.test.parameters.passwd.user}")
-    String userPasswd;
-
     @LocalServerPort
     private int port;
     
     @Autowired
     private TestRestTemplate restTemplate;
 
-
+    private String email = "jose@jose.com";
+    private String senha = "senhateste";
+    
     @Test 
     public void shouldSignUp () throws URISyntaxException{
         URI uri = Utils.getUriForPath("/signup", port);
         
         SignUpForm signUpForm = new SignUpForm();
-        signUpForm.setEmail("jose@jose.com");
+        signUpForm.setEmail(email);
         signUpForm.setName("ArrobaRotemaio");
-        signUpForm.setSenha("senhateste");
+        signUpForm.setSenha(senha);
 
         HttpEntity<SignUpForm> requestEntity = new HttpEntity<>(signUpForm);
         ResponseEntity<UserDto> result = this.restTemplate.postForEntity(uri, requestEntity, UserDto.class);
         
         assertThat(result.getStatusCodeValue()).isEqualTo(201);
         assertThat(result.getBody().getName().equals(signUpForm.getName()));
+    }
+
+    @Test
+    public void trySignIn() throws URISyntaxException {
+        URI uri = Utils.getUriForPath("/auth", port);
+        LoginForm loginForm = new LoginForm();
+        loginForm.setEmail(email);
+        loginForm.setSenha(senha);
+
+        HttpEntity<LoginForm> request = new HttpEntity<>(loginForm);
+        ResponseEntity<TokenDto> result = this.restTemplate.postForEntity(uri, request, TokenDto.class);
+
+        assertThat(result.getStatusCodeValue()).isEqualTo(200);
+        assertThat(result.getBody().getTipo()).containsIgnoringCase("Bearer");
+        assertThat(result.getBody()).getClass().equals(TokenDto.class);
     }
 }
